a69a0ce8742be2cc657c20c3e0eb6abb8e2c89a3
Azarolol <gimazovazamat@mail.ru>
2021-12-17 23:05:08 +0300

    Scripted auto-commit on change (2021-12-17 23:05:08) by gitwatch.sh

--- src/main/java/hdfs/lab5/azarolol/StressTestApp.java
+++ src/main/java/hdfs/lab5/azarolol/StressTestApp.java
@@ -63,7 +63,9 @@ public class StressTestApp {
                             storage,
                             new GetResultMessage(request.first()),
                             Duration.ofMillis(DURATION_TIME))
-                            .thenCompose(
+                            .thenCompose(response -> {
+                                
+                            }
                     )
                 })
         )
