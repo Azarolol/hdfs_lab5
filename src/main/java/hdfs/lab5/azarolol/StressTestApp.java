96a5c8a815d4b1a3b44d8491a49d74945e3b1700
Azarolol <gimazovazamat@mail.ru>
2021-12-18 00:30:19 +0300

    Scripted auto-commit on change (2021-12-18 00:30:19) by gitwatch.sh

--- src/main/java/hdfs/lab5/azarolol/StressTestApp.java
+++ src/main/java/hdfs/lab5/azarolol/StressTestApp.java
@@ -95,9 +95,6 @@ public class StressTestApp {
     }
 
     private static Sink<Pair<String, Integer>, CompletionStage<Long>> createSink() {
-        Sink<Integer, CompletionStage<Long>> fold = Sink.fold(
-                new SaveResultMessage("", 0L), SaveResultMessage
-        );
         return Flow.<Pair<String, Integer>>create()
                 .mapConcat(request ->
                         new ArrayList<>(Collections.nCopies(request.second(), request.first())))
